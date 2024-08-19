2a0a291f93bb1d07a91b00323907db3743255d4c
melihovodina <melihoxa@gmail.com>
2024-08-19 17:28:18 +0300

    made visual of my services section

--- client/src/sections/myServices/MyServices.tsx
+++ client/src/sections/myServices/MyServices.tsx
@@ -9,6 +9,8 @@ const MyServices = () => {
         <h1 className='myServices-title'>МОИ УСЛУГИ</h1>
         <ServicesList/>
       </div>
+      <img className='myServices-image-first' src="/images/service-flower.png" alt="first background flower on 'My services' page"/> 
+      <img className='myServices-image-second' src="/images/service-flower.png" alt="second background flower on 'My services' page"/> 
     </div>
   )
 }
