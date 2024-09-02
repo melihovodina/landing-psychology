b358415ee208c09cd562144e81ffedfcc0c2151d
melihovodina <melihoxa@gmail.com>
2024-09-02 11:00:57 +0300

    made visual structure of about me page

--- client/src/sections/aboutMe/AboutMe.tsx
+++ client/src/sections/aboutMe/AboutMe.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { aboutMeText } from '../../оther/texts'
 import './aboutMe.css'
 
 const AboutMe = () => {
   return (
     <div className='aboutMe-main'>
-        aboutMe
+      <div className='aboutMe-content center'>
+        <div className='aboutMe-top'>
+          <div className='photo-block'/>
+          <div className='aboutMe-textblock'>
+            <h1 className='aboutMe-title'>ОБО МНЕ</h1>
+            <p className='aboutMe-text'>{aboutMeText[0]}</p>
+          </div>
+        </div>
+        <div className='aboutMe-bottom'>
+          <div className='aboutMe-textblock'>
+            <p className='aboutMe-text'>{aboutMeText[1]}</p>
+          </div>
+          <div className='photo-block'/>
+        </div>
+      </div>
     </div>
   )
 }
