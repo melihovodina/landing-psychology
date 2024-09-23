66710d80f3aa3286766fa1df6e9fddd9d40fd17c
melihovodina <melihoxa@gmail.com>
2024-09-23 11:02:01 +0300

    education section is done

--- client/src/sections/education/Education.tsx
+++ client/src/sections/education/Education.tsx
@@ -1,10 +1,9 @@
 import React, { useContext } from 'react'
 import './education.css'
 import { educationText } from '../../оther/texts';
-import LeftArrow from '../../components/svg/LeftArrow';
-import RightArrow from '../../components/svg/RightArrow';
 import { DeviceContext } from '../../components/contexts/DeviceContext';
 import Slider from '../../components/slider/Slider';
+import LazyComponent from '../../components/LazyComponent';
 
 const Education = () => {
   const {isMobile} = useContext(DeviceContext);
@@ -13,15 +12,19 @@ const Education = () => {
     return (
       <div className='education-main'>
         <div className='education-content center'>
-          <h1 className='education-title'>ОБРАЗОВАНИЕ</h1>
-          <div className='education-subtitles'>
-            <h2 className='education-subtitle'>{educationText[0]}</h2>
-            <h2 className='education-subtitle'>{educationText[1]}</h2>
-          </div>
+          <LazyComponent height='15vh' width='5vw' threshold={0.9}>
+            <h1 className='education-title'>ОБРАЗОВАНИЕ</h1>
+            <div className='education-subtitles'>
+              <h2 className='education-subtitle'>{educationText[0]}</h2>
+              <h2 className='education-subtitle'>{educationText[1]}</h2>
+            </div>
+          </LazyComponent>
+          <LazyComponent height='10vh' width='5vw' threshold={0.9}>
             <Slider mainClass='education-slider-main' containerClass='education-slider-container' childClass='education-slider-child'>
               <img className='education-photo' src="/images/education-test-1.jpg" alt="Psychologist on main page"/> 
               <img className='education-photo' src="/images/education-test-2.jpg" alt="Psychologist on main page"/>
             </Slider>
+          </LazyComponent>  
         </div>
         <img className='education-image-first' src="/images/education-flower-1.png" alt="first background flower on about me page"/>
         <img className='education-image-third' src="/images/education-flower-2.png" alt="second background flower on about me page"/> 
@@ -32,15 +35,19 @@ const Education = () => {
   return (
     <div className='education-main'>
       <div className='education-content center'>
-        <h1 className='education-title'>ОБРАЗОВАНИЕ</h1>
-        <div className='education-subtitles'>
-          <h2 className='education-subtitle'>{educationText[0]}</h2>
-          <h2 className='education-subtitle'>{educationText[1]}</h2>
-        </div>
+        <LazyComponent height='10vh' width='5vw' threshold={0.9}>
+          <h1 className='education-title'>ОБРАЗОВАНИЕ</h1>
+          <div className='education-subtitles'>
+            <h2 className='education-subtitle'>{educationText[0]}</h2>
+            <h2 className='education-subtitle'>{educationText[1]}</h2>
+          </div>
+        </LazyComponent>
+        <LazyComponent height='20vh' width='5vw' threshold={0.9}>
           <div className='education-photos'>
             <img className='education-photo' src="/images/education-test-1.jpg" alt="Psychologist on main page"/> 
             <img className='education-photo' src="/images/education-test-2.jpg" alt="Psychologist on main page"/>
           </div>
+         </LazyComponent>
       </div>
       <img className='education-image-first' src="/images/education-flower-1.png" alt="first background flower on about me page"/>
       <img className='education-image-second' src="/images/education-flower-3.png" alt="third background flower on about me page"/>  
