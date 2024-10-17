1e23cee27420cbd7ce0fb9d27bf8277984e72f7b
melihovodina <melihoxa@gmail.com>
2024-10-17 10:58:55 +0300

    chaged fonts for pc, started rebuild about me section

--- client/src/sections/aboutMe/AboutMe.tsx
+++ client/src/sections/aboutMe/AboutMe.tsx
@@ -14,7 +14,7 @@ const AboutMe:FC = () => {
           <img className='aboutMe-photo-first' src="/images/knees.webp" alt="Psychologist on about me page"/> 
           <div className='aboutMe-textblock'>
             <h1 className='aboutMe-title'>ОБО МНЕ</h1>
-            <p className='aboutMe-text'>{aboutMeText[1]}</p>
+            <p className='aboutMe-text'>{aboutMeText[0]}</p>
           </div> 
         </div>
         <div className='aboutMe-bottom'>
@@ -32,17 +32,34 @@ const AboutMe:FC = () => {
     <div className='aboutMe-main'>
       <div className='aboutMe-content center'>
         <div className='aboutMe-top'>
-          <img className='aboutMe-photo-first' src="/images/knees.webp" alt="Psychologist on about me page"/> 
+          <div className='aboutMe-photo-block-first'>
+            <img className='aboutMe-photo-first' src="/images/knees.webp" alt="Psychologist on about me page"/> 
+          </div> 
           <div className='aboutMe-textblock'>
             <h1 className='aboutMe-title'>ОБО МНЕ</h1>
+            <p className='aboutMe-text'>{aboutMeText[0]}</p>
             <p className='aboutMe-text'>{aboutMeText[1]}</p>
+            <p className='aboutMe-text'>{aboutMeText[2]}</p>
+            <ul className='aboutMe-text'>
+              <li className='aboutMe-text'>{aboutMeText[3]}</li>
+              <li className='aboutMe-text'>{aboutMeText[4]}</li>
+              <li className='aboutMe-text'>{aboutMeText[5]}</li>
+              <li className='aboutMe-text'>{aboutMeText[6]}</li>
+              <li className='aboutMe-text'>{aboutMeText[7]}</li>
+              <li className='aboutMe-text'>{aboutMeText[8]}</li>
+              <li className='aboutMe-text'>{aboutMeText[9]}</li>
+            </ul>
           </div>
         </div>
         <div className='aboutMe-bottom'>
           <div className='aboutMe-textblock'>
-            <p className='aboutMe-text'>{aboutMeText[1]}</p>
+            <p className='aboutMe-text'>{aboutMeText[10]}</p>
+            <p className='aboutMe-text'>{aboutMeText[11]}</p>
+            <p className='aboutMe-text'>{aboutMeText[12]}</p>
           </div>
-          <img className='aboutMe-photo-second' src="/images/hair.webp" alt="Psychologist on about me page"/> 
+          <div className='aboutMe-photo-block-second'>
+            <img className='aboutMe-photo-second' src="/images/hair.webp" alt="Psychologist on about me page"/>
+          </div>  
         </div>
       </div>
     </div>
