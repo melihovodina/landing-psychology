4d9335f7806b16e88c38a1852bfcce9c0ed1cfad
melihovodina <melihoxa@gmail.com>
2025-01-02 16:33:28 +0300

    made mobile & tablet designs for services section

--- client/src/components/BurgerList.tsx
+++ client/src/components/BurgerList.tsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import BurgerItem from "./burgerItem/BurgerItem";
 import { servicesText } from "../оther/texts";
 
 const BurgerList: React.FC = () => {
   const [openIndex, setOpenIndex] = useState<number | null>(0);
+  const divRef = useRef<HTMLDivElement>(null);
+  const [maxHeight, setMaxHeight] = useState<number>(0);
+
+  useEffect(() => {
+    if (divRef.current) {
+      const currentHeight = divRef.current.offsetHeight;
+      if (currentHeight > maxHeight) {
+        setMaxHeight(currentHeight);
+      }
+    }
+  }, [openIndex, maxHeight]);
+
+  useEffect(() => {
+    if (divRef.current) {
+      const currentHeight = divRef.current.offsetHeight;
+      setMaxHeight(currentHeight);
+    }
+  }, []);
 
   const handleToggle = (index: number) => {
     if (openIndex !== index) {
@@ -12,7 +30,7 @@ const BurgerList: React.FC = () => {
   };
 
   return (
-    <div>
+    <div ref={divRef} style={{ minHeight: `${maxHeight}px` }}>
       {servicesText.map((item, index) => (
         <BurgerItem
           key={item.index}
